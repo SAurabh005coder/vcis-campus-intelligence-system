fa50e0d85dcc61e1baf1ccce5538441d234c058a
SAurabh005coder <saurabhsahucg@gmail.com>
2026-08-22 22:24:33 +0530

    feat: configure backend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from app.routers.students import router as students_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 
 from app.database import engine
@@ -7,6 +8,13 @@ from app.database import engine
 app = FastAPI()
 app.include_router(students_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
